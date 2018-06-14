d6bc32035938b5ab408ef78437d9803b64942b0b
Nusrat Nuriyev <nuriyevn@gmail.com>
2018-06-14 13:20:15 +0300

    install.sh added

---
+++ build/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install firewalld from universe
+# open ports 22 17990 80 443 17988
+#firewall-cmd --zone=public --add-port=22/tcp --permanent
+# firewall-cmd --reload
+# apt-get install ssh
+# service sshd restart
+# sudo su
+# passwd ubuntu
+# *****
+# ssh ubuntu@192.168.1.107
+ 
+add-apt-repository universe && apt-get -y update && sudo apt-get  -y install firewalld git vim build-essential cmake libuv1-dev libmicrohttpd-dev && git clone https://github.com/nuriyevn/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make && ./xmrig
