dcc175abd5fcdce4d401621f5aa17b00e7a9c488
nuriyevn <nuriyevn@gmail.com>
2018-07-06 23:17:06 +0300

    Update install.sh
    added overclocking  stuff

--- build/install.sh
+++ build/install.sh
@@ -10,4 +10,9 @@
 # *****
 # ssh ubuntu@192.168.1.107
  
-add-apt-repository universe && apt-get -y update && sudo apt-get  -y install firewalld git vim build-essential cmake libuv1-dev libmicrohttpd-dev && git clone https://github.com/nuriyevn/xmrig.git && cd xmrig && cd build && cmake .. && make && ./xmrig
+add-apt-repository universe && apt-get -y update && sudo apt-get  -y install firewalld git vim build-essential cmake libuv1-dev libmicrohttpd-dev && git clone https://github.com/nuriyevn/xmrig.git && cd xmrig && cd build && cmake .. && make 
+#overclocking
+apt-get install -y ncurses-dev ipmitool && cd ../.. && git clone http://github.com/turionpowercontrol/tpc && cd tpc && make && make install && modprobe cpuid && modprobe msr && tpc -psmax 1 && ipmitool sensor #last line show power consumption
+#apt-add-repository 'deb http://downloads.linux.hpe.com/SDR/repo/mcp xenial current/non-free'
+#apt-get install -y hp-health --allow-unauthenticated
+cd .. && cd xmrig && cd build && ./xmrig
