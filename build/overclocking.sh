d9fdf4a3efd724ac4c93cccf1f47f7153d365bc8
nuriyevn <nuriyevn@gmail.com>
2018-07-20 20:18:35 +0300

    Update overclocking.sh

--- build/overclocking.sh
+++ build/overclocking.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 sudo -s
-apt-get install -y ncurses-dev
-git clone http://github.com/turionpowercontrol/tpc
-cd tpc
-make
-make install
-modprobe cpuid
-modprobe msr
-tpc -psmax 1
+#apt-get install -y ncurses-dev
+#git clone http://github.com/turionpowercontrol/tpc
+#cd tpc
+#make
+#make install
+#modprobe cpuid
+#modprobe msr
+#tpc -psmax 1
 
-apt-add-repository 'deb http://downloads.linux.hpe.com/SDR/repo/mcp xenial current/non-free'
-apt-get install -y hp-health --allow-unauthenticated
+#apt-add-repository 'deb http://downloads.linux.hpe.com/SDR/repo/mcp xenial current/non-free'
+#apt-get install -y hp-health --allow-unauthenticated
 
-apt-get install ipmitool -y
-ipmitool sensor #last line show power consumption
+#apt-get install ipmitool -y
+#ipmitool sensor #last line show power consumption
